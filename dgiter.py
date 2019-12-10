0d437435fff882421747254c66bf90f08d8d3691
Christopher O'Grady <cpo@slac.stanford.edu>
2019-12-09 19:41:01 -0800

    dgram iteration for nersc

---
+++ dgiter.py
@@ -0,0 +1,35 @@
+from psana import DataSource
+import numbers
+import numpy as np
+ds = DataSource(files='data.xtc2')
+myrun = next(ds.runs())
+
+fundamental_types = [np.ndarray,numbers.Number,str]
+
+def iter(myitem,name):
+    if (isinstance(myitem,np.ndarray) or isinstance(myitem,numbers.Number) or isinstance(myitem,str) or isinstance(myitem,dict)):
+        if not isinstance(myitem,dict): # ignore the meta-data for enum's.
+            print(name,':\n',myitem)
+            name = '.'.join(name.split('.')[:-1])
+    else:
+        attrs = [a for a in dir(myitem) if not a.startswith('_')]
+        assert len(attrs)>0,myitem
+        for attr in attrs:
+            name+='.'+attr
+            iter(getattr(myitem,attr),name)
+            name = '.'.join(name.split('.')[:-1])
+
+def dump(dg):
+    attrs = [a for a in dir(dg) if not a.startswith('_') and a not in ['service','timestamp','software']]
+    for attr in attrs:
+        det = getattr(dg,attr)
+        if isinstance(det,dict):
+            for segment,myitem in det.items():
+                iter(myitem,attr+'.'+str(segment))
+
+print('******** configure')
+dump(myrun.configs[0])
+for nevt,evt in enumerate(myrun.events()):
+    print('******** event',nevt)
+    dump(evt._dgrams[0])
+    break
