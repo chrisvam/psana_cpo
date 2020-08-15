466184af8821db7c2392dfc7bd7273624159d235
Christopher O'Grady <cpo@slac.stanford.edu>
2020-08-14 23:59:24 -0700

    count how many config items we have compared

--- configcompare.py
+++ configcompare.py
@@ -24,17 +24,23 @@ def todict(obj,result,special_list):
                     result[a][i]={}
                     todict(attr(i),result[a][i],special_list)
 
-def myiter(d1,d2,mykeys):
-    for k, v in d1.items():
-        if isinstance(v, dict):
-            mykeys.append(k)
-            myiter(v,d2[k],mykeys)
-            del mykeys[-1]
-        else:
-            if type(v) is np.ndarray:
-                if not np.array_equal(v,d2[k]): print(mykeys+[k],v,d2[k])
+class compare:
+    def __init__(self,d1,d2):
+        self.mykeys=[]
+        self.ncheck=0
+        self.iterate(d1,d2)
+    def iterate(self,d1,d2):
+        for k, v in d1.items():
+            if isinstance(v, dict):
+                self.mykeys.append(k)
+                self.iterate(v,d2[k])
+                del self.mykeys[-1]
             else:
-                if v!=d2[k]: print(mykeys+[k],v,d2[k])
+                self.ncheck+=1
+                if type(v) is np.ndarray:
+                    if not np.array_equal(v,d2[k]): print(self.mykeys+[k],v,d2[k])
+                else:
+                    if v!=d2[k]: print(self.mykeys+[k],v,d2[k])
 
 expruns = [sys.argv[1],sys.argv[2]]
 allresults = []
@@ -46,4 +52,5 @@ for exprun in expruns:
     todict(ecfg,result,{'evr':1,'quad':4,'elemCfg':16,'adc':10,'asics':4})
     allresults.append(result)
 
-myiter(allresults[0],allresults[1],[])
+cmp = compare(allresults[0],allresults[1])
+print('Checked',cmp.ncheck,'values')
