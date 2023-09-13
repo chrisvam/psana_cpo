d828d4aa48ffa99a5bddb07ca3c40fe4d5fb03a7
Christopher Ogrady <cpo@sdfiana002.sdf.slac.stanford.edu>
2023-09-13 10:09:28 -0700

    add path to sinfo

--- checkffb.py
+++ checkffb.py
@@ -7,7 +7,7 @@ import datetime
 def gethosts(queue):
     openhosts = []
     otherhosts = []
-    result = subprocess.check_output('sinfo -N -p %s'%queue, shell=True).decode('utf-8')
+    result = subprocess.check_output('/opt/slurm/slurm-curr/bin/sinfo -N -p %s'%queue, shell=True).decode('utf-8')
     for line in result.split('\n')[1:]:
         if ' idle' in line or ' alloc' in line or ' mix' in line or ' drain' in line or ' down' in line:
             fields = line.split()
@@ -24,6 +24,7 @@ fname='/sdf/data/lcls/drpsrcf/ffb/test/psana_batch/e307-r0999-s00-c00.xtc'
 
 openhosts,otherhosts = gethosts('milano')
 nerr=0
+msg=''
 for nhost,host in enumerate(openhosts):
     cmd = 'ssh %s ls %s'%(host,fname)
     try:
@@ -31,6 +32,16 @@ for nhost,host in enumerate(openhosts):
         #print('****',host,result)
     except:
         nerr+=1
-        print('*ERR',host)
-print('--- Summary: found',len(openhosts),'nodes with',nerr,'errors')
+        msg+='*ERR %s\n'%host
+msg+='*** Summary: found %d nodes with %d errors\n'%(len(openhosts),nerr)
 
+from email.mime.text import MIMEText
+from subprocess import Popen, PIPE
+    
+mailmsg = MIMEText(msg)
+mailmsg["From"] = "cpo@slac.stanford.edu"
+import getpass
+mailmsg["To"] = getpass.getuser()+"@slac.stanford.edu"
+mailmsg["Subject"] = "ffb test"
+p = Popen(["/usr/sbin/sendmail", "-t"], stdin=PIPE)
+p.communicate(mailmsg.as_string().encode())
