3bb6e15cfbcf28d2ab4f86006ea2ce15d966c863
Christopher Ogrady <cpo@sdfiana004.sdf.slac.stanford.edu>
2025-04-01 18:13:35 -0700

    update lcls2 event counter script with detname

--- lcls2_event_count.py
+++ lcls2_event_count.py
@@ -4,15 +4,16 @@ import subprocess
 import os
 exp=sys.argv[1]
 myrunnum=int(sys.argv[2])
+detname=sys.argv[3]
 hutch=exp[:3]
 datadir = os.environ['SIT_PSDM_DATA']
 files=glob.glob(datadir+'/'+hutch+'/'+exp+'/xtc/smalldata/*r%4.4d*.xtc2'%myrunnum)
 for f in files:
     output = subprocess.run(['detnames',f],capture_output=True)
-    if 'timing' in output.stdout.decode("utf-8"):
+    if detname in output.stdout.decode("utf-8"):
         cmd = f'xtcreader -s -f {f}'
         output = subprocess.run(cmd.split(),capture_output=True)
         print(output.stdout.decode('utf-8'))
         sys.exit(0)
-print('Error: timing detector not found')
+print(f'Error: f{detname} detector not found')
 sys.exit(-1)
