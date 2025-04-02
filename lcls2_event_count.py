e50e64efef82efa2ad3ddba3a498645891bf59e5
Christopher Ogrady <cpo@sdfiana004.sdf.slac.stanford.edu>
2025-04-01 17:58:58 -0700

    update lcls2 event counter script

--- lcls2_event_count.py
+++ lcls2_event_count.py
@@ -1,14 +1,16 @@
 import glob
 import sys
 import subprocess
+import os
 exp=sys.argv[1]
 myrunnum=int(sys.argv[2])
 hutch=exp[:3]
-files=glob.glob('/sdf/data/lcls/ds/'+hutch+'/'+exp+'/xtc/smalldata/*r%4.4d*.xtc2'%myrunnum)
+datadir = os.environ['SIT_PSDM_DATA']
+files=glob.glob(datadir+'/'+hutch+'/'+exp+'/xtc/smalldata/*r%4.4d*.xtc2'%myrunnum)
 for f in files:
     output = subprocess.run(['detnames',f],capture_output=True)
     if 'timing' in output.stdout.decode("utf-8"):
-        cmd = f'xtcreader -C -f {f}'
+        cmd = f'xtcreader -s -f {f}'
         output = subprocess.run(cmd.split(),capture_output=True)
         print(output.stdout.decode('utf-8'))
         sys.exit(0)
