17bb99ddcbc12cab067630368e691b1a37c4eb36
Christopher Ogrady <cpo@sdfiana004.sdf.slac.stanford.edu>
2025-04-01 17:54:37 -0700

    update lcls2 event counter script

--- lcls2_event_count.py
+++ lcls2_event_count.py
@@ -1,10 +1,16 @@
 import glob
 import sys
-import os
+import subprocess
 exp=sys.argv[1]
 myrunnum=int(sys.argv[2])
 hutch=exp[:3]
 files=glob.glob('/sdf/data/lcls/ds/'+hutch+'/'+exp+'/xtc/smalldata/*r%4.4d*.xtc2'%myrunnum)
 for f in files:
-    os.system('detnames %s'%f)
-    os.system('xtcreader -f %s | grep L1Accept | wc -l'%f)
+    output = subprocess.run(['detnames',f],capture_output=True)
+    if 'timing' in output.stdout.decode("utf-8"):
+        cmd = f'xtcreader -C -f {f}'
+        output = subprocess.run(cmd.split(),capture_output=True)
+        print(output.stdout.decode('utf-8'))
+        sys.exit(0)
+print('Error: timing detector not found')
+sys.exit(-1)
